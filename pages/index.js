ec6b6398dd8d48de12d1dccdf8939167f6110ea0
CMS209 <darkwolfshadow21@gmail.com>
2025-04-07 11:26:33 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Act of God</h1>
+      <p>This is a test website for the Act of God clothing brand.</p>
+    </div>
+  );
+};
+
+export default Home;
