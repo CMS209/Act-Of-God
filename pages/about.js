ea52ca3bf9ae6a86c17ed88c8d7828b97bbc35f5
CMS209 <darkwolfshadow21@gmail.com>
2025-04-07 11:34:17 -0700

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Act of God</h1>
+      <p>Act of God is a clothing brand that stands for resilience, creativity, and innovation. Established in 2025.</p>
+    </div>
+  );
+};
+
+export default About;
