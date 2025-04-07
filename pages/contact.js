a38ce61954d4fb454459c10a5fab4ef3bc0e88f4
CMS209 <darkwolfshadow21@gmail.com>
2025-04-07 11:34:34 -0700

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Email: actofgod@example.com</p>
+      <p>Follow us on Instagram, Twitter, and Facebook!</p>
+    </div>
+  );
+};
+
+export default Contact;
