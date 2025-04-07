9136ba6cae33252963df182608d9eb3c6e4acf60
CMS209 <darkwolfshadow21@gmail.com>
2025-04-07 11:31:42 -0700

    Create shop.js

---
+++ pages/shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>Shop - Act of God</h1>
+      <p>Explore our collection of exclusive clothing.</p>
+      <ul>
+        <li>Hoodie - $50</li>
+        <li>T-Shirt - $30</li>
+        <li>Cap - $20</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Shop;
